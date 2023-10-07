Enovale/DPP

commit 2f2946954d19558b120b146ec058e2b526b2411c

build: fix #926, speedrun to #1000 (#927)

src/dpp/discordevents.cpp
@@ -100,12 +100,12 @@ void set_snowflake_array_not_null(const json* j, const char *keyname, std::vecto
 	}
 }
 
-void for_each_json(const nlohmann::json* parent, const char* key, std::function<void(const nlohmann::json*)> fn) {
+void for_each_json(nlohmann::json* parent, std::string_view key, const std::function<void(nlohmann::json*)> &fn) {
 	auto it = parent->find(key);
 	if (it == parent->end() || it->is_null()) {
 		return;
 	}
-	for (const nlohmann::json &elem : *parent) {
+	for (nlohmann::json &elem : *parent) {
 		fn(&elem);
 	}
 }

include/dpp/discordevents.h
@@ -23,6 +23,8 @@
 #include <dpp/export.h>
 #include <dpp/json_fwd.h>
 #include <dpp/json_interface.h>
+#include <string_view>
+#include <functional>
 
 namespace dpp {
 
@@ -53,7 +55,7 @@ void DPP_EXPORT set_snowflake_array_not_null(const nlohmann::json* j, const char
  * @param key key name to check for the values
  * @param fn function to apply to each element
  */
-void DPP_EXPORT for_each_json(nlohmann::json* parent, std::string_view key, std::function<void(const nlohmann::json*)> fn);
+void DPP_EXPORT for_each_json(nlohmann::json* parent, std::string_view key, const std::function<void(nlohmann::json*)> &fn);
 
 /** @brief Sets an array of objects from a json field value, if defined, else does nothing
  * @tparam T The class of which the array consists of. Must be derived from dpp::json_interface

commit 541aa4e7b6dee08df6f04947dbfe1ebf09f25c97

fix: fix bug introduced by #927 (#928)

src/dpp/discordevents.cpp
@@ -105,7 +105,7 @@ void for_each_json(nlohmann::json* parent, std::string_view key, const std::func
 	if (it == parent->end() || it->is_null()) {
 		return;
 	}
-	for (nlohmann::json &elem : *parent) {
+	for (nlohmann::json &elem : *it) {
 		fn(&elem);
 	}
 }